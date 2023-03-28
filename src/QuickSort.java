88b66c3e467a802ad05241cd06c158b0c3e3dc20
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-03-28 23:54:37 +0530

    Quick

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,4 +1,23 @@
 public class QuickSort {
+
+//    QuickSort: Quick sort is another popular sorting algorithm that also uses the divide-and-conquer approach to sort
+//               list of elements. The basic idea behind quick sort is to choose a pivot element from the list, partition
+//               the list into two sub-lists such that elements smaller than the pivot are on the left, and elements
+//               greater than the pivot are on the right, and then recursively apply quick sort to each sub-list.
+
+//               Here are the steps involved in the quick sort algorithm:
+
+//                    1. Choose a pivot element from the input list. The pivot can be any element, but commonly the first
+//                       or last element of the list is chosen.
+//                    2. Partition the list into two sub-lists: elements smaller than the pivot are on the left, and elements
+//                       greater than the pivot are on the right.
+//                    3. Recursively apply quick sort to each sub-list, until they are sorted.
+//                    4. Concatenate the sorted left sub-list, the pivot, and the sorted right sub-list to produce a
+//                       fully sorted list.
+
+
+
+
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
