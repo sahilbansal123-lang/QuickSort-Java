1f2f0fe3a08a2eda8cdc0050518c232d0ce7d09d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-03-29 00:30:14 +0530

    Quick

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -9,17 +9,56 @@ public class QuickSort {
 
 //                    1. Choose a pivot element from the input list. The pivot can be any element, but commonly the first
 //                       or last element of the list is chosen.
-//                    2. Partition the list into two sub-lists: elements smaller than the pivot are on the left, and elements
-//                       greater than the pivot are on the right.
+//                    2. Partition the list into two sub-lists: elements smaller than the pivot are on the left, and
+//                       elements greater than the pivot are on the right.
 //                    3. Recursively apply quick sort to each sub-list, until they are sorted.
 //                    4. Concatenate the sorted left sub-list, the pivot, and the sorted right sub-list to produce a
 //                       fully sorted list.
 
+//                Time complexity:  Worst Case: O(n^2)
+//                                  Average Case: O(nlogn)
 
+    private static int partition(int[] arr, int low, int high) {
 
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++){
+            if (arr[j] < pivot){
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+//      arr[high] = pivot position
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if(low < high) {
+
+//          pidx = pivot index that is coming from partition
+            int pidx = partition(arr, low, high);
+            quickSort(arr, low, pidx - 1);
+            quickSort(arr, pidx + 1, high);
+
+        }
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int arr[] = {6, 3, 9, 5, 2, 8};
+        int n = arr.length;
+        quickSort(arr, 0, n-1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
